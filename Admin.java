df8fc96a5b6a23485e98572084c5484b9726056a
Sam <sampaisley3@gmail.com>
2026-03-14 18:06:55 +0000

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,18 +1,6 @@
 package greentrees;
 public class Admin extends Person {
-
-    private String adminPass;
-
     public Admin(String name, int userID, String password) {
         super(name, userID, password);
-        this.adminPass = adminPass;
-    }
-
-    public String getAdminPass() {
-        return adminPass;
-    }
-
-    public void setAdminPass(String adminPass) {
-        this.adminPass = adminPass;
     }
 }
