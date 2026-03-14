3c110c2c01aedb46914b70d6cc655b63f2a6aefe
Sam <sampaisley3@gmail.com>
2026-03-14 18:06:37 +0000

    working login

--- GreenTreesApp.java
+++ GreenTreesApp.java
@@ -1,8 +1,22 @@
 package greentrees;
 
-public class GreenTreesApp {
+import java.util.ArrayList;
 
+public class GreenTreesApp {
+    public static ArrayList<Admin> admin = new ArrayList<>();
+    public static ArrayList<Volunteer> volunteer = new ArrayList<>();
+    
     public static void main(String[] args) {
         
+        //populating Arrays
+        
+        //admin
+        admin.add(new Admin("Admin",1,"admin123"));
+        
+        //volunteers
+        volunteer.add(new Volunteer("Joe",101,"pass123","5 Parklane drive","0895003231"));
+                
+        
+        new LoginFormGUI().setVisible(true);
     }
 }
